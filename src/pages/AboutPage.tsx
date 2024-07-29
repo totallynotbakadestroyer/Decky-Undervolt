ddf936c3e11e7f976d7012bfe3a59c5178d8a9bf
bakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2024-07-29 13:32:42 +0500

    update AboutPage.tsx

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -12,6 +12,7 @@
                 <li><b>deadwenk (Alexey Tarasov)</b>: For contributing Steam-Deck-Software-Undervolt and making it easier to use per-core undervolting</li>
                 <li><b>FoxN</b>: For finding a solution to make software undervolting possible at Steam Deck OLED</li>
                 <li><b>Robert (biddbb)</b>: For maintaining overclocking guide</li>
+                <li><b>NGnius</b>: For resolving licensing issues</li>
                 <li><b>Everyone in the UltraNX Steam Deck community</b>: For their support and feedback</li>
             </ul>
             <div style={{textAlign: 'center'}}>
