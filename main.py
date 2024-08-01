29c904883b8528680a979f871cf8ead1b0efd22f
bakadestroyer <52473158+totallynotbakadestroyer@users.noreply.github.com>
2024-08-01 18:16:08 +0500

    Add proper logging

--- main.py
+++ main.py
@@ -25,13 +25,12 @@ DEFAULT_SETTINGS = {
 class Plugin:
 
     async def init(self):
+        decky_plugin.logger.info('Initializing plguin...')
         for key in DEFAULT_SETTINGS:
             if settings.getSetting(key) is None:
-                settings.setSetting(key, DEFAULT_SETTINGS[key])
-        if settings.getSetting("status") == 'Enabled':
-            if(settings.getSetting("timeout_before_enable") is not None):
-                sleep(settings.getSetting("timeout_before_enable"))
-                Plugin.set_undervolt(self, settings.getSetting("cores"), False)     
+                decky_plugin.logger.info(f"Setting {key} to default value")
+                settings.setSetting(key, DEFAULT_SETTINGS[key])   
+        decky_plugin.logger.info('Plugin initialized')        
 
     def calculate_hex_value(core, value):
         core_shifted = hex(core * 0x100000)
@@ -41,12 +40,14 @@ class Plugin:
     
 
     async def disable_undervolt(self):
+        decky_plugin.logger.info('Disabling undervolt')
         for core, value in enumerate([0, 0, 0, 0]):
             hex_value = Plugin.calculate_hex_value(core, value)
             subprocess.run(["sudo", RYZENADJ_CLI_PATH, f"--set-coper={hex_value}"], cwd=defaultDir)
         settings.setSetting("status", 'Disabled');
 
     async def apply_undervolt(self, core_values, timeout):
+        decky_plugin.logger.info(f'Applying undervolt with negative values: {core_values} and timeout: {timeout}')
         if timeout is not None and timeout > 0:
             await sleep(timeout)
         cores = [-value for value in core_values]
@@ -61,12 +62,16 @@ class Plugin:
                 )
                 stdout = result.stdout
                 stderr = result.stderr
-                decky_plugin.logger.debug(stdout)
-                decky_plugin.logger.debug(stderr)
+                if stdout:
+                    decky_plugin.logger.info(f"RYZENADJ: {stdout}")
+                if stderr:
+                    decky_plugin.logger.error(f"RYZENADJ: {stderr}")
         settings.setSetting("status", 'Enabled')
         await sleep(2)
-    
+        decky_plugin.logger.info('Undervolt applied')
+
     async def save_preset(self, preset):
+        decky_plugin.logger.info(f'Saving preset: {preset}')
         presets = settings.getSetting("presets")
         for existing_preset in presets:
             if existing_preset["app_id"] == preset["app_id"]:
@@ -79,28 +84,36 @@ class Plugin:
             "use_timeout": preset["use_timeout"]
         })
         settings.setSetting("presets", presets)
+        decky_plugin.logger.info('Preset saved')
 
     async def save_settings(self, newSettings):
+        decky_plugin.logger.info(f'Saving settings: {newSettings}')
         settings.setSetting("settings", newSettings)
 
     async def save_setting(self, key, value):
+        decky_plugin.logger.info(f'Saving setting: {key} with value: {value}')
         settings.setSetting(key, value);
 
     async def get_setting(self, key):
+        decky_plugin.logger.info(f'Getting setting: {key}')
         return settings.getSetting(key)
     
     async def reset_config(self):
+        decky_plugin.logger.info('Resetting config')
         for key in DEFAULT_SETTINGS:
             settings.setSetting(key, DEFAULT_SETTINGS[key])
         return DEFAULT_SETTINGS
 
     async def fetch_config(self):
+        decky_plugin.logger.info('Fetching config')
         config = {}
         for key in DEFAULT_SETTINGS:
             config[key] = settings.getSetting(key)
+        decky_plugin.logger.info(f'Config fetched: {config}')    
         return config    
     
     async def update_preset(self, preset):
+        decky_plugin.logger.info(f'Updating preset: {preset}')
         presets = settings.getSetting("presets")
         for existing_preset in presets:
             if existing_preset["app_id"] == preset["app_id"]:
@@ -112,6 +125,7 @@ class Plugin:
 
     async def delete_preset(self, app_id):
         presets = settings.getSetting("presets")
+        decky_plugin.logger.info(f'Deleting preset with app_id: {app_id} from presets {presets}')
         for existing_preset in presets:
             if existing_preset["app_id"] == app_id:
                 presets.remove(existing_preset)
